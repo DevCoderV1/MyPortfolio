a949628a62e24d5f0408b0449f218c6645317266
M1ck3yZ <m1ck3yz@hotmail.com>
2021-06-23 23:29:33 -0400

    Update FrontPage.js

--- portfolio/src/components/FrontPage.js
+++ portfolio/src/components/FrontPage.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
+import Projectlist from "./Projectlist";
 import myself from "../images/myself.jpg";
 import java from "../images/javascript.png";
 import html from "../images/html.png";
