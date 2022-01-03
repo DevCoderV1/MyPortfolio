6d6b7fb0d02d9fe61a3cbaca01e5a9c181399f7a
M1ck3yZ <m1ck3yz@hotmail.com>
2022-01-03 15:20:03 -0500

    Making appearance changes in the portfolio
    -change font size

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState } from 'react'
 const Project = ({
   _project_name,
   _project_link,
   _project_github,
   _project_image,
 }) => {
-  const [display, setdisplay] = useState(false);
+  const [display, setdisplay] = useState(false)
 
   return (
     <div
@@ -13,26 +13,26 @@ const Project = ({
       onMouseMove={() => setdisplay(true)}
       onMouseLeave={() => setdisplay(false)}
     >
-      <img src={_project_image} alt="test1" />
-      <div className={display ? "show_project_btn" : "hide_project_btn"}>
+      <img src={_project_image} alt="" />
+      <div className={display ? 'show_project_btn' : 'hide_project_btn'}>
         <h1>{_project_name}</h1>
         <button
           onClick={() => {
-            window.open(`${_project_github}`);
+            window.open(`${_project_github}`)
           }}
         >
           GitHub
         </button>
         <button
           onClick={() => {
-            window.open(`${_project_link}`);
+            window.open(`${_project_link}`)
           }}
         >
           View Project
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Project;
+export default Project
